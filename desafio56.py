1fd85acf512896914e10e95d22f536f7a577c7c1
MatheusNespolo <nespolo.m1@gmail.com>
2024-09-11 17:18:59 -0300

    desafio56
    Commit e era isso!

--- desafio56.py
+++ desafio56.py
@@ -1,10 +1,26 @@
-nome = list = ['','','','']
-idade = list = ['','','','']
-sexo = list = ['','','','']
+somaidade = 0
+mediaidade = 0
+idade = 0
+idadeh = 0
+mulhermenor20 = 0
+for p in range(1,5):
+    nome = str(input('Digite o nome da pessoa {}: '.format(p)))
+    idade = int(input('Digite a idade da pessoa {}: '.format(p)))
+    sexo = input('Selecione o sexo - M ou F: ')
+    somaidade =+ idade
+    if p == 1 and sexo in 'Mm':
+        nomeh = nome
+        idadeh = idade
+    if sexo in 'Mm' and idade > idadeh:
+        idadeh = idade
+        nomeh = nome
+    else:
+        nomeh = 'nenhum'
+    if sexo in 'Ff' and idade < 20:
+        mulhermenor20 += 1
+    
 
-for c in range(0,4):
-    nome[c] = str(input('Digite o nome da pessoa {}: '.format(c+1)))
-    idade[c] = int(input('Digite a idade da pessoa {}: '.format(c+1)))
-    sexo[c] = input('Selecione o sexo - M ou F: ')
-print('A média entre as idades é: {}'.format((sum(idade))/4))
-print(nome[max(idade[0])])
+mediaidade = somaidade/4
+print('A média entre as idades é: {} anos.'.format(mediaidade))
+print('O homem mais velho é: {}.'.format(nomeh))
+print('O número de mulheres menores de 20 anos é: {}'.format(mulhermenor20))
