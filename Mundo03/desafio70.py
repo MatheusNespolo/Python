496beacfd4275929229ba45adfa00617116e3ec9
MatheusNespolo <nespolo.m1@gmail.com>
2024-09-18 13:12:14 -0300

    Até desafio 70 ok
    Commit e era isso! Obrigado Colab, obrigado Gemini.

--- Mundo03/desafio70.py
+++ Mundo03/desafio70.py
@@ -1,16 +1,24 @@
+precos = nomes = []
+precos = [int(x) for x in precos]
 sair = bool = False
 soma = caro = 0
 while not sair:
     nome = str(input('Nome do produto: '))
+    nomes.append(nome)
     preco = float(input('Preço do produto: '))
+    precos.append(preco)
     if preco > 1000:
-        caro +=1
+        caro += 1
     soma += preco
     cont = str(input('Continuar? (S/N) '))
     if cont in 'Nn':
         sair == True
+        break
     elif cont in 'Ss':
         sair == False
     else:
         sair == True
-print(f'a) {soma}\nb) {}')
+        break
+barato = min(precos)
+indice = precos.index(barato)
+print(f'a) {soma}\nb) {caro}\nc) {nomes[indice]}')
