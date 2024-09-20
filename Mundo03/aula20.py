f82cc7699530086a48f6255e5a791deec3afa66c
MatheusNespolo <nespolo.m1@gmail.com>
2024-09-20 09:16:09 -0300

    exemplo de função
    Commit e era isso!

---
+++ Mundo03/aula20.py
@@ -0,0 +1,15 @@
+def mostraLinha():
+    print('-/'*30)
+mostraLinha()
+print('         SISTEMA DE ALUNOS')
+mostraLinha()
+print('         CADASTRO DE FUNCIONÁRIOS')
+mostraLinha()
+print('         ERRO DO SISTEMA')
+
+def mensagem(msg):
+    mostraLinha()
+    print(msg)
+    mostraLinha()
+
+mensagem('Mensagem padrão')
