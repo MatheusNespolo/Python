c8331f5a36f723d70c8fc26a51720194a25fd0ee
MatheusNespolo <nespolo.m1@gmail.com>
2024-09-26 08:36:05 -0300

    parte 2 da aula 17
    Commit e era isso! Fazer os exercícios depois. Vo iniciar com HTML hoje.

---
+++ Mundo03/aula17p2.py
@@ -0,0 +1,8 @@
+galera = list()
+dado = list()
+for cont in range(0,4):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])
+    dado.clear()
+print(galera)
