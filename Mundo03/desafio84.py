8de82cf42cf817b5c67dc80531032a7571557763
MatheusNespolo <nespolo.m1@gmail.com>
2024-09-26 15:24:11 -0300

    Desafio 84 CEV
    Commit e era isso!

---
+++ Mundo03/desafio84.py
@@ -0,0 +1,17 @@
+peso = list()
+dado = list()
+cadastro = 0
+cont = 0
+while True:
+    dado.append(str(input('Nome: ')))
+    peso.append(int(input('Peso: ')))
+    cadastro += 1
+    esc = str(input('Quer continuar? [S/N] '))
+    if esc in 'Nn':
+        print('Fim dos cadastros.')
+        break
+    elif esc in 'Ss':
+        print('Cadastre mais pessoas.')
+print(f'Ao todo, você cadastrou {cadastro} pessoas.')
+print(f'O maior peso foi de {max(peso)}Kg.')
+print(f'O menor peso foi de {min(peso)}Kg.')
