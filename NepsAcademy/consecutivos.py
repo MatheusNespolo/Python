fba864c95a2061ffb0c2077786623939c2ca4b2d
MatheusNespolo <nespolo.m1@gmail.com>
2024-10-01 11:36:25 -0300

    Exercício cansado
    Commit e era isso! Usei a ajuda do Gemini para criar uma função :massage:

--- NepsAcademy/consecutivos.py
+++ NepsAcademy/consecutivos.py
@@ -1,13 +1,22 @@
 #Consecutivos
-lista = []
-consec = 0
+def calcular_pontos(valores):
+  if not valores:
+    return 0
+
+  sequencia_atual = 1
+  maior_sequencia = 1
+
+  for i in range(1, len(valores)):
+    if valores[i] == valores[i - 1]:
+      sequencia_atual += 1
+    else:
+      sequencia_atual = 1
+
+    maior_sequencia = max(maior_sequencia, sequencia_atual)
+
+  return maior_sequencia
+
 N = int(input())
-lista.append(input().split())
-len(lista) == N
-def achar_consecutivos():
-    for i in lista:
-        if lista[0][i] == lista[0][i+1]:
-            consec += 1
-    return consec()
-print(lista)
-print(consec)
+valores = list(map(int, input().split()))
+pontos = calcular_pontos(valores)
+print(pontos)
