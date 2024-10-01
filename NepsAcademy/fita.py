215f9c96b653570b4d4cf5f8504f8f3b31233cf3
MatheusNespolo <nespolo.m1@gmail.com>
2024-10-01 14:59:34 -0300

    Exercício da fita
    To chegando em algum lugar. Commit e era isso!

--- NepsAcademy/fita.py
+++ NepsAcademy/fita.py
@@ -1,9 +1,33 @@
 #Fita Colorida
+def proximo_pintado(valores):
+    if not valores:
+        return 0
+
+    for i in range(0, len(valores)):
+        if i == 0:
+            if valores[i] == -1:
+                index = valores.index(0)
+                saida.append(index)
+        elif i == 1:
+            if valores[i] == valores[i-1]:
+                saida.append(index-i)
+        elif i == 2:
+            if valores[i] == 0:
+                saida.append(0)
+        elif i > 2 and i < (len(valores)/2):
+            if valores[i] == -1:
+                index = valores[:i].index(0)
+                saida.append(i-index)
+            else:
+                saida.append(0)
+        else:
+            index = valores[i:].index(0)
+            saida.append(i-index)
+
+    return saida
+
 saida = []
 N = int(input())
 valores = list(map(int, input().split()))
-for i in range(1, len(valores)):
-    if valores[i] == 0:
-        saida.append(0)
-    elif valores[i] < 0:
-        saida.append(valores.index(0))
+colorida = proximo_pintado(valores)
+print(colorida)    
