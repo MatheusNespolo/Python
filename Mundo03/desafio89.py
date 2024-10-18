3ae0abc0abfa7085de6e35b1962934640f37b838
MatheusNespolo <nespolo.m1@gmail.com>
2024-10-18 16:06:31 -0300

    python da tarde
    Commit e era isso!

--- Mundo03/desafio89.py
+++ Mundo03/desafio89.py
@@ -1,11 +1,5 @@
-lista = []
-while True:
-    lista.append(str(input('Nome: ')))
-    lista.append(int(input('Nota 1: ')))
-    lista.append(int(input('Nota 2: ')))
-    esc = str(input('Quer continuar? [S/N] '))
-    if esc in 'Nn':
-        print('='*30)
-        break
-for i, pessoas in enumerate(lista):
-    print(i, pessoas)
+ficha = list()
+
+nome = str('Nomes: ')
+nota1 = str('Nota 1: ')
+nota2 = str('Nota 2 : ')
