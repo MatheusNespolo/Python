ef8e9dfbd4e426ea232671b3e7530a188f77b5e5
MatheusNespolo <nespolo.m1@gmail.com>
2024-10-22 10:10:58 -0300

    desafio 89
    Exercício de listas compostas (importante!). Commit e era isso!

--- Mundo03/desafio89.py
+++ Mundo03/desafio89.py
@@ -1,5 +1,23 @@
 ficha = list()
-
-nome = str('Nomes: ')
-nota1 = str('Nota 1: ')
-nota2 = str('Nota 2 : ')
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    resp = str(input('Quer continuar? (S/N) '))
+    if resp in 'Nn':
+        break
+print('-='*30)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('-'*26)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-'*35)
+    opc = int(input('Mostrar notas de qual aluno? (999 fecha). '))
+    if opc == 999:
+        print('Finalizado.')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
