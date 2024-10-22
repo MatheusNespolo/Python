c2e28c18accf88ed91938d0896591ea304d9344b
MatheusNespolo <nespolo.m1@gmail.com>
2024-10-22 14:52:16 -0300

    Python dps
    Commit e era isso!

---
+++ Mundo03/desafio91.py
@@ -0,0 +1,13 @@
+import random
+resultados = {
+    'jogador1': random.randint(1,6),
+    'jogador2': random.randint(1,6),
+    'jogador3': random.randint(1,6),
+    'jogador4': random.randint(1,6)
+}
+final = resultados[].sort()
+print(final)
+#print(f'1º Lugar: {}')
+#print(f'2º Lugar: {}')
+#print(f'3º Lugar: {}')
+#print(f'4º Lugar: {}')
