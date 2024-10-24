883a98dbb3fdedd33f9ac8350fe315ea20cdc803
MatheusNespolo <nespolo.m1@gmail.com>
2024-10-24 16:38:58 -0300

    Desafio 94
    Commit e era isso!

--- Mundo03/desafio94.py
+++ Mundo03/desafio94.py
@@ -6,32 +6,29 @@ idades = []
 acima = []
 cadastros = []
 
-cadastro = {
-    'Nome': '',
-    'Sexo': '',
-    'Idade': ''
-}
+pessoa = {}
 
 while True:
-    nome = str(input('Digite o nome: '))
-    nomes.append(nome)
-    sexo = str(input('Digite o sexo: (F/M) '))
-    sexos.append(sexo)
-    if sexo == 'F':
-        mulheres.append(nome)
-    idade = int(input('Digite a idade: '))
-    idades.append(idade)
-    resp = str(input('Continuar? (S/N) '))
+    pessoa.clear()
+    pessoa['Nome'] = str(input('Digite o nome: '))
+    while True:
+        pessoa['Sexo'] = str(input('Digite o sexo: (F/M) ')).upper()[0]
+        sexos.append(pessoa['Sexo'])
+        if pessoa['Sexo'] == 'F':
+            mulheres.append(pessoa['Nome'])
+        if pessoa['Sexo'] in 'FM':
+            break
+        print('ERRO! Digite F ou M para o sexo.')
+    pessoa['Idade'] = int(input('Digite a idade: '))
+    idades.append(pessoa['Idade'])
+    cadastros.append(pessoa.copy())
+    resp = str(input('Continuar? (S/N) ')).upper()[0]
     cont += 1
     if resp in 'Nn':
         print('Cadastro finalizado!')
         break
-
+print('-='*30)
 media = (sum(idades) / cont)
-
-for nome in range nomes:
-    cadastro['Nome'] = nomes[nome]
-    cadastros.append(cadastro.items)
 
 for j in idades:
     if j > media:
