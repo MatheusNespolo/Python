5f4c759768bb714471268f0f242703bb436fab16
MatheusNespolo <nespolo.m1@gmail.com>
2024-10-24 17:28:22 -0300

    Primeiro desafio de função
    Commit e era isso!

---
+++ Mundo03/desafio96.py
@@ -0,0 +1,7 @@
+def area(l, c):
+    area = l * c
+    print(f'A área do terreno de {l}x{c} é de {area}m².')
+
+l = int(input('Digite a largura do terreno: '))
+c = int(input('Digite o comprimento do terreno: '))
+area(l, c)
