bcaf76954b8803e328d7951795bda9670378335b
MatheusNespolo <nespolo.m1@gmail.com>
2024-10-29 09:12:34 -0300

    Função contador do CEV
    Desafio 98. Commit e era isso!

--- Mundo03/desafio98.py
+++ Mundo03/desafio98.py
@@ -1,7 +1,32 @@
-def contador(inicio, fim, passo):
-    for i in range(inicio, fim, passo):
-        print(i)
+from time import sleep
 
-contador(1, 11, 1)
+
+def contador(i, f, p):
+    print(f'Contagem de {i} até o {f} de {p} em {p}.')
+    print('-='*20)
+    sleep(2.5)
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='', flush=True) #flush impede a bufferização da contagem
+            sleep(0.5)
+            cont += p
+        print('Fim!')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ', end='', flush=True) #flush impede a bufferização da contagem
+            sleep(0.5)
+            cont -= p
+        print('Fim!')
+    print('-='*20)
+
+contador(1, 10, 1)
 
 contador(10, 0, 2)
+
+print('Agora personalize a contagem: ')
+ini = int(input('Insira o início da contagem: '))
+fim = int(input('Insira o final da contagem: '))
+pas = int(input('Insira o passo da contagem: '))
+contador(ini, fim, pas)
