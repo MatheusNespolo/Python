9c73cc0e1f4c80d7f34c8fff59e452110f6b2550
MatheusNespolo <nespolo.m1@gmail.com>
2024-10-31 09:22:04 -0300

    Correção desafio 101
    Commit e era isso!

--- Mundo03/desafio101.py
+++ Mundo03/desafio101.py
@@ -1,13 +1,13 @@
-from datetime import datetime
 
 def voto(a):
+    from datetime import datetime
     ano_atual = datetime.today().year
     idade = ano_atual - a
-    if idade < 18:
+    if idade < 16:
         print(f'Com {idade} anos: NÃO VOTA.')
-    elif 18 < idade < 70:
+    elif 18 <= idade < 70:
         print(f'Com {idade} anos: VOTO OBRIGATÓRIO.')
-    elif idade > 70:
+    elif 16 <= idade < 18 or idade > 65:
         print(f'Com {idade} anos: VOTO OPCIONAL.')
     else:
         print('Ano de nascimento inválido.')
