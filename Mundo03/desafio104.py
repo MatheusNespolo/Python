8a9e8a42336e83d808fefc0356715493a585514d
MatheusNespolo <nespolo.m1@gmail.com>
2024-10-31 10:52:15 -0300

    Atualização desafio 104 CEV
    Commit e era isso!

--- Mundo03/desafio104.py
+++ Mundo03/desafio104.py
@@ -1,9 +1,18 @@
 import colorama
 
-def leiaInt(num):
-    if type(num) == int:
-        print(f'Você acabou de digitar o número {num}.')
-    else:
-        print('ERRO! Digite um número válido.')
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print(colorama.Back.RED + 'ERRO! Digite um número inteiro válido.' + colorama.Style.RESET_ALL)
+        if ok:
+            break
+    return valor
 
 n = leiaInt('Digite um número: ')
+print(colorama.Fore.BLUE + f'Você acabou de digitar o número {n}.' + colorama.Style.RESET_ALL)
