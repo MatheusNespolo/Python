ab742d2c2e9d8774f4abae0d8d18654b431fb257
MatheusNespolo <nespolo.m1@gmail.com>
2024-11-11 16:33:24 -0300

    desafio 111 do Cev
    Commit e era isso! Visual Studio Code não necessariamente entende que uma pasta = um módulo. Portanto a importação é feita diretamente dentro da pasta (Ex.: import moeda, sendo que, moeda está {desafio111})

---
+++ Mundo03/desafio111/utilidadesCeV/dado/__init__.py
@@ -0,0 +1,53 @@
+def aumentar(n, por):
+    aumento = (por / 100) * n
+    return aumento + n
+
+def diminuir(n, por):
+    queda = (por / 100) * n
+    return n - queda
+
+def dobro(n):
+    return n * 2
+
+def metade(n):
+    return n / 2
+
+def aumentar(n, por, formata = True):
+    aumento = (por / 100) * n
+    n = aumento + n
+    if formata == True:
+        n = int(n)
+        n = str(n)
+        n = 'R$'+n+',00'
+    return n
+
+def diminuir(n, por, formata = True):
+    queda = (por / 100) * n
+    n = n - queda
+    if formata == True:
+        n = int(n)
+        n = str(n)
+        n = 'R$'+n+',00'
+    return n
+
+def dobro(n, formata = True):
+    n = n * 2
+    if formata == True:
+        n = int(n)
+        n = str(n)
+        n = 'R$'+n+',00'
+    return n
+
+def metade(n, formata = True):
+    n = n / 2
+    if formata == True:
+        n = int(n)
+        n = str(n)
+        n = 'R$'+n+',00'
+    return n
+
+def moeda(n):
+    n = int(n)
+    n = str(n)
+    n = 'R$'+n+',00'
+    return n
