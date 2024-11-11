4cda180e6cd48121f346e11d99506bc5da39d21a
MatheusNespolo <nespolo.m1@gmail.com>
2024-11-11 17:08:05 -0300

    Desafio 112 Cev
    Tratamento da entrada criando a função leiaMoeda() dentro do módulo dado e importando-o para o código.
    É necessário transformar a entrada em uma str para então aplicar a função isdigit() e posteriormente transforma-la em float

---
+++ Mundo03/desafio112/utilidadesCeV/desafio112.py
@@ -0,0 +1,5 @@
+import moeda
+import dado
+
+p = dado.leiaMoeda()
+moeda.resumo(p, 15, 68)
---
+++ Mundo03/desafio112/utilidadesCeV/moeda/__init__.py
@@ -0,0 +1,10 @@
+def resumo(n, aum, red):
+    n = float(n)
+    print('-'*40)
+    print(f'    RESUMO DO VALOR')
+    print('-'*40)
+    print(f'Preço analisado:    R${n:.0f},00')
+    print(f'Dobro do preço:     R${2*n:.0f},00')
+    print(f'Metade do preço:    R${n/2:.0f},00')
+    print(f'80% de aumento:     R${n + (n*(aum/100)):.0f},00')
+    print(f'30% de redução:     R${n - (n*(red/100)):.0f},00')
