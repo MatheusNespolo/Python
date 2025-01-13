8c4f97210eff21cceccc7b3b2a1f96c7ad47c91c
MatheusNespolo <nespolo.m1@gmail.com>
2025-01-13 11:15:39 -0300

    Adicionei mais um except
    Commit e era isso!

--- Mundo03/desafio113/desafio113.py
+++ Mundo03/desafio113/desafio113.py
@@ -9,8 +9,12 @@ def leiaInt(msg):
                 ok = True
         except (ValueError, TypeError):
             print(colorama.Back.RED + 'ERRO! Digite um número inteiro válido.' + colorama.Style.RESET_ALL)
+        except KeyboardInterrupt:
+            print('O usuário escolheu sair do programa.')
+            break
         if ok:
             break
+    return n
 
 def leiaFloat(msg):
     ok = False
@@ -21,9 +25,13 @@ def leiaFloat(msg):
                 ok = True
         except (ValueError, TypeError):
             print(colorama.Back.RED + 'ERRO! Digite um número real válido.' + colorama.Style.RESET_ALL)
+        except KeyboardInterrupt:
+            print('O usuário escolheu sair do programa.')
+            break
         if ok:
             break
+    return m
 
 n = leiaInt('Digite um inteiro: ')
 m = leiaFloat('Digite um real: ')
-# print(colorama.Fore.BLUE + f'Você acabou de digitar o número {n}.' + colorama.Style.RESET_ALL)
+print(colorama.Fore.BLUE + f'Você acabou de digitar o número {n} (inteiro) e {m} (real).' + colorama.Style.RESET_ALL)
