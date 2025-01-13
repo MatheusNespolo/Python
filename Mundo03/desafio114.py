4218525e0a46dd9475b1c7f5b7568c0e825755b5
MatheusNespolo <nespolo.m1@gmail.com>
2025-01-13 11:45:23 -0300

    Teste vo reabrir o desafio 114
    Commit e era isso!

--- Mundo03/desafio114.py
+++ Mundo03/desafio114.py
@@ -3,17 +3,17 @@ import colorama
 
 def verificar_site(url):
     try:
-        resposta = requests.get({url}, timeout=5)
+        resposta = requests.get(url, timeout=5)
         if resposta.status_code == 200:
             print(f"O site {url} está acessível!")
         else:
             print(f"O site {url} retornou o código de status {resposta.status_code}.")
     except requests.ConnectionError:
-        print(colorama.Back.RED + f"Não foi possível conectar ao site {url}." + colorama.Style.RESET_ALL)
+        print(f"Não foi possível conectar ao site {url}.")
     except requests.Timeout:
-        print(colorama.Back.RED + f"O tempo de conexão com o site {url} expirou." + colorama.Style.RESET_ALL)
+        print(f"A conexão com o site {url} expirou.")
     except Exception as e:
-        print(colorama.Back.RED + f"Ocorreu um erro ao verificar o site {url}: {e}" + colorama.Style.RESET_ALL)
+        print(f"Ocorreu um erro ao verificar o site {url}: {e}")
 
 # Teste
 verificar_site("https://www.pudim.com.br")
