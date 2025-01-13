4161ffb36a14c96228a099db116f372338ffd596
MatheusNespolo <nespolo.m1@gmail.com>
2025-01-13 12:22:13 -0300

    O site pudim não está acessível
    Mas apenas no Python do VScode. No meu navegador abre normalmente. É possível fazer o teste importando e usando a biblioteca requests ou então com as urllib. Avaliar melhor método.

--- Mundo03/desafio114.py
+++ Mundo03/desafio114.py
@@ -1,19 +1,11 @@
-import requests
-import colorama
+import urllib
+import urllib.request
 
-def verificar_site(url):
-    try:
-        resposta = requests.get(url, timeout=5)
-        if resposta.status_code == 200:
-            print(f"O site {url} está acessível!")
-        else:
-            print(f"O site {url} retornou o código de status {resposta.status_code}.")
-    except requests.ConnectionError:
-        print(f"Não foi possível conectar ao site {url}.")
-    except requests.Timeout:
-        print(f"A conexão com o site {url} expirou.")
-    except Exception as e:
-        print(f"Ocorreu um erro ao verificar o site {url}: {e}")
+site = str(input('Insira o site: '))
 
-# Teste
-verificar_site("https://www.pudim.com.br")
+try:
+    site1 = urllib.request.urlopen('https://'+site+'/')
+except urllib.request.URLError:
+    print('O site não está acessível.')
+else:
+    print('O site está acessível.')
