ac3c1ce47cb76db0f4c6cde955a07a79e7b6c959
MatheusNespolo <nespolo.m1@gmail.com>
2025-01-13 11:11:23 -0300

    arrumei o desafio 113
    Commit e era isso!
    Usei os loops while para garantir que a varíavel digitada seja do tipo desejado.

--- Mundo03/desafio113/desafio113.py
+++ Mundo03/desafio113/desafio113.py
@@ -2,35 +2,27 @@ import colorama
 
 def leiaInt(msg):
     ok = False
-    valor = 0
     while True:
         try:
             n = int(input(msg))
+            if isinstance(n, int):
+                ok = True
         except (ValueError, TypeError):
             print(colorama.Back.RED + 'ERRO! Digite um número inteiro válido.' + colorama.Style.RESET_ALL)
-        n = str(msg)
-        if n.isnumeric():
-            valor = int(n)
-            ok = True
         if ok:
             break
-    return valor
 
 def leiaFloat(msg):
-    okF = False
-    valorF = 0
+    ok = False
     while True:
         try:
             m = float(input(msg))
+            if isinstance(m, float):
+                ok = True
         except (ValueError, TypeError):
             print(colorama.Back.RED + 'ERRO! Digite um número real válido.' + colorama.Style.RESET_ALL)
-        m = str(msg)
-        if m.isnumeric():
-            valorF = float(m)
-            okF = True
-        if okF:
+        if ok:
             break
-    return valorF
 
 n = leiaInt('Digite um inteiro: ')
 m = leiaFloat('Digite um real: ')
