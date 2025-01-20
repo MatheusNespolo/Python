8ea5ed22b9c6dbaa5c1eb98141061241c724ece9
MatheusNespolo <nespolo.m1@gmail.com>
2025-01-20 15:40:28 -0300

    Fuga finalizado
    Commit e era isso. Minha solução não atualiza a posição do fugitivo.

--- NepsAcademy/fuga.py
+++ NepsAcademy/fuga.py
@@ -1,10 +1,36 @@
 #Fuga
 
-posicoes = list(map(int, input().split()))
+H, P, F, D = list(map(int, input().split()))
 
-H = posicoes[0]
-P = posicoes[1]
-F = posicoes[2]
-D = posicoes[3]
+if D == 1:  # anti-horário
+    pos_f = F
+    while True:
+        pos_f = (pos_f + 1) % 16
+        if pos_f == H:
+            print('S')
+            break
+        elif pos_f == P:
+            print('N')
+            break
+else:   # horário
+    pos_f = F
+    while True:
+        pos_f = (pos_f - 1 + 16) % 16
+        if pos_f == H:
+            print('S')
+            break
+        elif pos_f == P:
+            print('N')
 
-# Não finalizado
+#  Não atualiza as posições do fugitivo
+
+# if H < P and P < F or H > P and P < F:
+#     if D == 1:
+#         print('S')
+#     else:
+#         print('N')
+# elif H < P and P > F or H > P and P > F:
+#     if D == -1:
+#         print('S')
+#     else:
+#         print('N')
