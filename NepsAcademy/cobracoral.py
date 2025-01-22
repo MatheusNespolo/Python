b3f9fb5ab02e8adfab508c0db2b23e3375d0223c
MatheusNespolo <nespolo.m1@gmail.com>
2025-01-22 15:21:05 -0300

    atualização cobra coral
    Consegui no mó suor. Faltavam verificações e a visualização da resposta precisou da criação de uma variável. Commit e era isso!

--- NepsAcademy/cobracoral.py
+++ NepsAcademy/cobracoral.py
@@ -1,14 +1,17 @@
 #Cobra Coral
 
 p = list(map(int, input().split())) # Cria e mapeia o vetor como uma lista "p"
+res = ''
 
-for i in range(len(p)): # Percorre o vetor
-    p.count(p[i]) # Conta quantas vezes o valor p[i] aparece no vetor
-    if p.count(p[i]) > 1: # Se o valor aparecer mais de uma vez
-        dife = (p.index(p[i])) - (p.index(p[i])+1) # Subtrai a posição do primeiro valor encontrado pela posição do segundo valor encontrado
-    if dife > 1:
-        print('F')
-        break
-    else:
-        print('V')
-        break
+if res != 'F' and res != 'V': # Se a resposta não for F ou V
+    for i in range(len(p)): # Percorre o vetor
+        p.count(p[i]) # Conta quantas vezes o valor p[i] aparece no vetor
+        if p.count(p[i]) == 2:
+            vez1 = p.index(p[i])
+            vez2 = p.index(p[i], vez1 + 1) # Procura a segunda ocorrência do valor p[i]
+            if vez2 - vez1 == 2:
+                res = 'V'
+            else:
+                res = 'F'
+
+print(res)
