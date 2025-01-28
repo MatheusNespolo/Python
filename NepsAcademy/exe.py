f8f89e39ee1ba1ea377bd4f6e7df19745a903337
MatheusNespolo <nespolo.m1@gmail.com>
2025-01-28 08:38:01 -0300

    exe criado
    Apenas aquecendo no pomodoro de Python. Commit e era isso!

---
+++ NepsAcademy/exe.py
@@ -0,0 +1,11 @@
+#Resolvendo um Exercício de Programação
+
+P, R = map(int, input().split())
+
+
+if P == 0:
+    print('C')
+if P == 1 and R == 0:
+    print('B')
+if P == 1 and R == 1:
+    print('A')
