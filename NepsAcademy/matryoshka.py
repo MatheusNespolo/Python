b93ee8644a4f166ecbeb3c6d0ae7cdef69ec7c57
MatheusNespolo <nespolo.m1@gmail.com>
2025-02-03 09:10:54 -0300

    atualizei mas não finalizei
    Commit e era isso!

--- NepsAcademy/matryoshka.py
+++ NepsAcademy/matryoshka.py
@@ -8,13 +8,12 @@ mo = sorted(m)
 recolher = 0
 recolhidos = []
 
-for i in m:
-    i == i
-
-for j in mo:
-    if i != j:
+for i in range(0, len(m)):
+    if i != mo[i]:
         recolher += 1
-        recolhidos.append(j)
+        recolhidos.append(m[i])
+    elif i == mo[i]:
+        recolher = recolher
 
 print(recolher)
 for k in recolhidos:
