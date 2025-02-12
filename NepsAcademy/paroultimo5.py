53a9daf1210613f41ae57c73e2d25ee9fffbb981
MatheusNespolo <nespolo.m1@gmail.com>
2025-02-12 17:41:13 -0300

    .
    Commit e era isso!

--- NepsAcademy/paroultimo5.py
+++ NepsAcademy/paroultimo5.py
@@ -4,24 +4,9 @@ X = int(input())
 Y = int(input())
 Z = int(input())
 
-pares = 0
-finais = 0
+valores = [sorted(X, Y, Z)]
 
-def par(num):
-    if num % 2 == 0:
-        pares += 1
-    return pares
-
-def final5(num):
-    if num % 5 == 0:
-        finais += 1
-    return finais
-
-par(X)
-par(Y)
-par(Z)
-final5(X)
-final5(Y)
-final5(Z)
+for _ in range(len(valores)):
+    
 
 print(pares + finais)
