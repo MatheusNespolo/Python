d0071a8d5dbdd82eabda7f732693dfdad979871c
MatheusNespolo <nespolo.m1@gmail.com>
2025-02-14 08:53:51 -0300

    finalizei
    Commit e era isso!

--- NepsAcademy/paroultimo5.py
+++ NepsAcademy/paroultimo5.py
@@ -4,9 +4,12 @@ X = int(input())
 Y = int(input())
 Z = int(input())
 
-valores = [sorted(X, Y, Z)]
+final = 0
+
+valores = [X, Y, Z]
 
 for _ in range(len(valores)):
-    
+    if valores[_] % 2 == 0 or valores[_] % 5 == 0:
+        final += 1
 
-print(pares + finais)
+print(final)
