667c1be0e5d5eb67936a109579e32221131f0010
MatheusNespolo <nespolo.m1@gmail.com>
2025-03-12 14:48:43 -0300

    teste
    Vamo que vai dar bom

---
+++ Codility/non-empty-array.py
@@ -0,0 +1,21 @@
+# A non-empty array A consisting of N integers is given
+
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # Implement your solution here
+    contador = 0;
+    indice = 0;
+    indicador = False;
+    while not indicador:
+        contador += 1
+        indice = A[indice]
+        if contador > len(A):
+            indicador = True
+        if contador == len(A):
+            return -1
+        if indice == -1:
+            return contador
+    pass
+print(solution([1, 4, -1, 3, 2]))
