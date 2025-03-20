f39283c9cf5bcd856106ea12b44b5400bf9102b1
MatheusNespolo <nespolo.m1@gmail.com>
2025-03-20 14:01:20 -0300

    tentativa 521624.92
    Commit e era isso! ainda vou terminar. obrigado codeium.

--- NepsAcademy/copa2.py
+++ NepsAcademy/copa2.py
@@ -1,7 +1,7 @@
 #Copa do Mundo (OBI 2010)
 
 #Entrada
-placar = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0, 'P': 0}
+paises = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
 resultadosN = []
 resultadosM = []
 
@@ -10,7 +10,42 @@ for i in range(0, 15):
     resultadosN.append(N)
     resultadosM.append(M)
 
-#Processamento
-for i in range(0, 15):
-    if resultadosN[i] > resultadosM[i]:
+#Processamento das oitavas
+def oitavas():
+    oitavas = []
+    for i in range(0, 7):
+        if resultadosN[0] > resultadosM[0] and 'A' not in oitavas:
+            oitavas.append('A')
+        elif resultadosN[0] < resultadosM[0] and 'B' not in oitavas:
+            oitavas.append('B')
+        if resultadosN[1] > resultadosM[1] and 'C' not in oitavas:
+            oitavas.append('C')
+        elif resultadosN[1] < resultadosM[1] and 'D' not in oitavas:
+            oitavas.append('D')
+        if resultadosN[2] > resultadosM[2] and 'E' not in oitavas:
+            oitavas.append('E')
+        elif resultadosN[2] < resultadosM[2] and 'F' not in oitavas:
+            oitavas.append('F')
+        if resultadosN[3] > resultadosM[3] and 'G' not in oitavas:
+            oitavas.append('G')
+        elif resultadosN[3] < resultadosM[3] and 'H' not in oitavas:
+            oitavas.append('H')
+        if resultadosN[4] > resultadosM[4] and 'I' not in oitavas:
+            oitavas.append('I')
+        elif resultadosN[4] < resultadosM[4] and 'J' not in oitavas:
+            oitavas.append('J')
+        if resultadosN[5] > resultadosM[5] and 'K' not in oitavas:
+            oitavas.append('K')
+        elif resultadosN[5] < resultadosM[5] and 'L' not in oitavas:
+            oitavas.append('L')      
+        if resultadosN[6] > resultadosM[6] and 'M' not in oitavas:
+            oitavas.append('M')    
+        elif resultadosN[6] < resultadosM[6] and 'N' not in oitavas:
+            oitavas.append('N')
+        if resultadosN[7] > resultadosM[7] and 'O' not in oitavas:
+            oitavas.append('O')
+        elif resultadosN[7] < resultadosM[7] and 'P' not in oitavas:
+            oitavas.append('P')
+    
+
         
