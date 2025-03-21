4cefdebd74bf9fc237e512aaa1887b438102bb14
MatheusNespolo <nespolo.m1@gmail.com>
2025-03-21 09:39:20 -0300

    fazenda
    esse exercício me faz pensar: será que há um jeito ainda mais simples? apesar de ter simplificado a entrada pensei em implementar uma função para o processamento, mas será que valeria a pena? Commit e era isso!

--- NepsAcademy/fazenda.py
+++ NepsAcademy/fazenda.py
@@ -1,3 +1,16 @@
 #  Plantação de Morangos
 #  https://neps.academy/br/exercise/53
 
+# Entrada
+dados = []
+for i in range(4):
+    n = int(input())
+    dados.append(n)
+ 
+# Processamento
+area1 = dados[0] * dados[1]
+area2 = dados[2] * dados[3]
+maiorArea = (max(area1, area2))
+
+# Saída
+print(maiorArea)
