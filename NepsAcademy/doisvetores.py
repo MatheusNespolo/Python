19e1e0507753b558b521555b70e72b67b8095a1a
MatheusNespolo <nespolo.m1@gmail.com>
2025-03-24 09:10:29 -0300

    tranquilo
    Commit e era isso!

--- NepsAcademy/doisvetores.py
+++ NepsAcademy/doisvetores.py
@@ -1,2 +1,18 @@
 #Dois Vetores: Pares e Ímpares
 #https://neps.academy/br/exercise/194
+
+pares = []
+impares = []
+
+for i in range(10):
+    num = int(input())
+    if num % 2 == 0:
+        pares.append(num)
+    else:
+        impares.append(num)
+
+for numero in pares:
+    print(numero, end=' ')
+print()
+for numero in impares:
+    print(numero, end=' ')
