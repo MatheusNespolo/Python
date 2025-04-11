5b97356d31bb1fe8a8e588c5828ad7bb0e2b15ab
MatheusNespolo <nespolo.m1@gmail.com>
2025-04-11 13:35:36 -0300

    retorno
    Commit e era isso!

--- NepsAcademy/dentista.py
+++ NepsAcademy/dentista.py
@@ -2,25 +2,25 @@
 #https://neps.academy/br/exercise/248
 
 #Entrada
-horarios = []
-duracao = []
 consultas = 1
+inicios = []
+terminos = []
 N = int(input())
-
 for i in range(N):
-    inicio, termino = map(int, input().split())
-    duracao.append(termino - inicio)
-    horarios.append(inicio)
-    horarios.append(termino)
+    X, Y = map(int, input().split())
+    inicios.append(X)
+    terminos.append(Y)
 
 #Processamento
-for j in range(N*2):
-    if j % 2 == 0:
-        if horarios[j] >= horarios[j-1]:
-            consultas += 1
-        if horarios[j] < horarios[j-1]:
-            horarios.remove(horarios[j])
-            consultas = consultas
+for j in range(1, N):
+    if inicios[j] >= terminos[j-1]:
+        consultas += 1
+    if inicios[j] < terminos[j-1]:
+        inicios.remove(inicios[j])
+        terminos.remove(terminos[j])
+        consultas = consultas
 
-#Saída
+#Saída
+print(inicios)
+print(terminos)
 print(consultas)
