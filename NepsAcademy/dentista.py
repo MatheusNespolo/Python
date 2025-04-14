91543fe72e3f37af09044b42612d37cfa5762243
MatheusNespolo <nespolo.m1@gmail.com>
2025-04-14 15:24:48 -0300

    Seu código falhou um exemplo de entrada.  Quando você digitar a seguinte entrada no seu programa:
    5
    10 30
    20 40
    30 60
    40 80
    60 100
    Resposta: 3

--- NepsAcademy/dentista.py
+++ NepsAcademy/dentista.py
@@ -4,28 +4,24 @@
 #Entrada
 inicios = []
 terminos = []
-consultas = 0
 N = int(input())
 for i in range(N):
     X, Y = map(int, input().split())
     inicios.append(X)
     terminos.append(Y)
+inicios.remove(inicios[0])
 
 #Processamento
 def marcar_consultas(inicios, terminos):
-    inicios.remove(inicios[0])
     consultas = 1
+    desmarcadas = 0
     for inicio in inicios:
-        if consultas == N:
-                break
         for termino in terminos:
-            if consultas == N:
-                break
             if inicio >= termino:
                 consultas += 1
-            if inicio < termino:
-                break
-    return consultas
+            if (inicio - termino) < 0:
+                desmarcadas += 1
+    return consultas - desmarcadas
 
 #Saída
 print(marcar_consultas(inicios, terminos))
