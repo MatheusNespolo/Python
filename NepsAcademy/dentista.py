889acf31a4dec258724c8365baac95b110993ba5
MatheusNespolo <nespolo.m1@gmail.com>
2025-04-15 11:23:13 -0300

    tentativa do uso de filas
    RuntimeError: deque mutated during iteration. Commit e era isso!

--- NepsAcademy/dentista.py
+++ NepsAcademy/dentista.py
@@ -1,27 +1,29 @@
 #Dentista
 #https://neps.academy/br/exercise/248
 
+#Bibliotecas
+from collections import deque
+
 #Entrada
-inicios = []
-terminos = []
+inicios = deque()
+terminos = deque()
 N = int(input())
 for i in range(N):
     X, Y = map(int, input().split())
     inicios.append(X)
     terminos.append(Y)
-inicios.remove(inicios[0])
+inicios.popleft()
 
 #Processamento
 def marcar_consultas(inicios, terminos):
-    consultas = 1
-    desmarcadas = 0
+    consultas = N
     for inicio in inicios:
         for termino in terminos:
             if inicio >= termino:
                 consultas += 1
             if (inicio - termino) < 0:
-                desmarcadas += 1
-    return consultas - desmarcadas
+                inicios.popleft()
+    return consultas
 
 #Saída
 print(marcar_consultas(inicios, terminos))
