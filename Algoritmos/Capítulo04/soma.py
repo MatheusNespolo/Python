9986ca26a4b1b1234d490849e676dc0e10c9b2b6
MatheusNespolo <nespolo.m1@gmail.com>
2025-04-22 12:20:04 -0300

    código para exercícios
    Soma de uma lista utilizando recursão. Commit e era isso!

---
+++ Algoritmos/Capítulo04/soma.py
@@ -0,0 +1,20 @@
+#Dividir para conquistar
+#Página 77
+
+# Neps
+# #Soma dos Elementos
+# N = int(input())
+# valores = list(map(int, input().split()))
+# print(sum(valores))
+
+#Guia ilustrado
+def soma(lista):
+    resultado = 0
+    if len(lista) < 2:
+        return lista
+    else:
+        for i in lista:
+            resultado += i
+        return resultado
+
+print(soma(lista=[2, 4, 6]))
