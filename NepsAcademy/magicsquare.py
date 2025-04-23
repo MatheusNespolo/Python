82d894c208369d70f28e9c92a4c4a8426e3dee88
MatheusNespolo <nespolo.m1@gmail.com>
2025-04-23 15:28:58 -0300

    foi com 3 funções
    Uma forma e verifica linhas outra forma e verifica colunas e outra verifica diagonais. a variável boleana recebe o resultado de uma função and entre as 3. Ótimo exemplo de dividir para conquistar. Commit e era isso!

--- NepsAcademy/magicsquare.py
+++ NepsAcademy/magicsquare.py
@@ -2,12 +2,60 @@
 #https://neps.academy/br/exercise/198
 
 linhas = []
+linha1 = []
+linha2 = []
+linha3 = []
+quadrado_magico = bool
 
-for i in range(9):
-    linha1 = []
-    linha2 = []
-    linha3 = []
-    if i <= 3:
+def forma_linhas():
+    for _ in range(3):
         linha1.append(int(input()))
-    if i > 3 and i <= 6:
+    for _ in range(3):
         linha2.append(int(input()))
+    for _ in range(3):
+        linha3.append(int(input()))
+    if sum(linha1) != sum(linha2) != sum(linha3):
+        quadrado_magico = False
+    else:
+        quadrado_magico = True
+    return quadrado_magico
+
+linhas.append(linha1)
+linhas.append(linha2)
+linhas.append(linha3)
+
+def forma_colunas():
+    coluna1 = []
+    coluna2 = []
+    coluna3 = []
+    for i in range(3):
+        coluna1.append(linhas[i][0])
+    for i in range(3):
+        coluna2.append(linhas[i][1])
+    for i in range(3):
+        coluna3.append(linhas[i][2])
+    if sum(coluna1) != sum(coluna2) != sum(coluna3):
+        quadrado_magico = False
+    else:
+        quadrado_magico = True
+    return quadrado_magico
+
+def verifica_diagonais():
+    diagonal_principal = []
+    diagonal_secundaria = []
+    for i in range(3):
+        diagonal_principal.append(linhas[i][i])
+    for i in range(3):
+        diagonal_secundaria.append(linhas[i][2-i])
+    if sum(diagonal_principal) != sum(diagonal_secundaria):
+        quadrado_magico = False
+    else:
+        quadrado_magico = True
+    return quadrado_magico
+
+quadrado_magico = forma_linhas() and forma_colunas() and verifica_diagonais()
+
+if quadrado_magico == True:
+    print('SIM')
+else:
+    print('NAO')
