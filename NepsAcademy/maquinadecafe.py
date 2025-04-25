46b7dd1298d8a6b10e12c484e1a6c9e04524b646
MatheusNespolo <nespolo.m1@gmail.com>
2025-04-25 09:46:09 -0300

    simplifiquei mais
    Ainda sim nada. Commit e era isso!

--- NepsAcademy/maquinadecafe.py
+++ NepsAcademy/maquinadecafe.py
@@ -5,10 +5,7 @@
 A1 = int(input())
 A2 = int(input())
 A3 = int(input())
-FuncionariosPorAndar = []
-FuncionariosPorAndar.append(A1)
-FuncionariosPorAndar.append(A2)
-FuncionariosPorAndar.append(A3)
+FuncionariosPorAndar = [A1, A2, A3]
 
 #Processamento e Saída
 AndarDaMaquina = FuncionariosPorAndar.index(max(FuncionariosPorAndar))
@@ -16,7 +13,7 @@ FuncionariosPorAndar.remove(max(FuncionariosPorAndar))
 
 if AndarDaMaquina == 0:
     print(A2 * 2 + A3 * 4)
-if AndarDaMaquina == 1:
+if AndarDaMaquina == 1 or A1 == A2 or A2 == A3:
     print(A1 * 2 + A3 * 2)
 if AndarDaMaquina == 2:
     print(A1 * 4 + A2 * 2)
