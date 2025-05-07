194de4eba61ad87ea42ac59ab3cdec7abfbb0b28
MatheusNespolo <nespolo.m1@gmail.com>
2025-05-07 09:24:24 -0300

    apenas criei
    Commit e era isso!

--- NepsAcademy/matue.py
+++ NepsAcademy/matue.py
@@ -1,3 +1,11 @@
 #Andando no Tempo
 #https://neps.academy/br/exercise/111
 
+A, B, C = map(int, input().split())
+
+if A == B or A == C or B == C:
+    print('S')
+if A == (B + C) or B == (A + C) or C == (A + B):
+    print('S')
+else:
+    print('N')
