ff61997142903f939640b04087a4acefeab36ee1
MatheusNespolo <nespolo.m1@gmail.com>
2025-05-12 09:37:18 -0300

    teleférico finalizado
    A matemática é uma ferramenta assim como a linguagem de programação. É necessário saber quando exatamente é possível resolver utilizando a matemática e posteriormente avaliar onde implementar a lógica de programação em si. Commit e era isso!

--- NepsAcademy/teleferico.py
+++ NepsAcademy/teleferico.py
@@ -1,13 +1,37 @@
 #Teleférico
 #https://neps.academy/br/exercise/15
 
+# Versão matemática + condicional (apresentou erro)
+# C = int(input())
+# A = int(input())
+# viagens = 0
+
+# if C >= (A + 1):
+#     viagens = 1
+# elif C < (A + 1):
+#     viagens = ((A + 1) // C)
+
+# print(viagens)
+
+# Versão recursiva (apresentou erro)
+# C = int(input())
+# A = int(input())
+# viagens = 0
+
+# if A <= (C - 1):
+#     viagens = 1
+# else:
+#     while A > 0:
+#         A = A - (C - 1)
+#         viagens += 1
+
+# print(viagens)
+
+# Versão matemática
 C = int(input())
 A = int(input())
-viagens = 0
 
-if C >= (A + 1):
-    viagens = 1
-elif C < (A + 1):
-    viagens = ((A + 1) // C)
+capacidade = C - 1
+viagens = (A + capacidade - 1) // capacidade
 
 print(viagens)
