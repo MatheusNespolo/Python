b21f75498baacf1cf04907a06645f7159b7f08f9
MatheusNespolo <nespolo.m1@gmail.com>
2025-05-15 15:30:46 -0300

    só comecei
    bgl ta treta fio aqui vai umas lista uns arrays pilha hash. mas calma que cpa vai até algoritmo. Commit e era isso filho!

--- NepsAcademy/ogros.py
+++ NepsAcademy/ogros.py
@@ -1,2 +1,19 @@
 # Ogros
 # https://neps.academy/br/exercise/261
+
+N, M = map(int, input().split())
+# N = quantidade de faixas de premiações
+# M = quantidade de ogros
+
+for i in range(N - 1):
+    
+
+forca = 0
+pontuacao = 0
+
+if forca >= 0 and forca >= 5:
+    pontuacao = 10
+elif forca >= 6 and forca <= 10:
+    pontuacao = 30
+
+
