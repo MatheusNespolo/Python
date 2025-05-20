6e1042cf4a6679decb0d505ba31ff374012ae166
MatheusNespolo <nespolo.m1@gmail.com>
2025-05-20 15:48:16 -0300

    nada de nada
    Commit e era isso!

--- NepsAcademy/ogros.py
+++ NepsAcademy/ogros.py
@@ -5,12 +5,14 @@ N, M = map(int, input().split())
 # N = quantidade de faixas de premiações
 # M = quantidade de ogros
 
-forca = 0
-pontuacao = 0
+limites = list(map(int, input().split()))
 
-if forca >= 0 and forca >= 5:
-    pontuacao = 10
-elif forca >= 6 and forca <= 10:
-    pontuacao = 30
+premiacoes = list(map(int, input().split()))
 
+forcas = list(map(int, input().split()))
 
+for forca in forcas:
+    if forca >= 0 and forca >= 5:
+        pontuacao = 10
+    elif forca >= 6 and forca <= 10:
+        pontuacao = 30
