5d193f663ee49e04eb68f3dbb760a7519fe349c7
MatheusNespolo <nespolo.m1@gmail.com>
2025-06-03 08:48:03 -0300

    .
    .

--- NepsAcademy/trocacartas.py
+++ NepsAcademy/trocacartas.py
@@ -4,9 +4,17 @@
 # Entrada
 
 A, B = map(int, input().split())
-cartasAlices = list(map(int, input().split()))
+cartasAlice = list(map(int, input().split()))
 cartasBeatriz = list(map(int, input().split()))
+trocas = 0
 
 # Processamento
 
+menorConjunto = min(A, B)
+
+for i in range(menorConjunto):
+    if cartasAlice[i] not in cartasBeatriz:
+        trocas += 1
+
+#Saída
 
