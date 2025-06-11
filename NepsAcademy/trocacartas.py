afd0b4d76aab025bd4061da3aed24e984b6a966c
MatheusNespolo <nespolo.m1@gmail.com>
2025-06-11 09:04:41 -0300

    atualizado trocacartas
    Commit e era isso!

--- NepsAcademy/trocacartas.py
+++ NepsAcademy/trocacartas.py
@@ -2,7 +2,6 @@
 #https://neps.academy/br/exercise/278
 
 # Entrada
-
 A, B = map(int, input().split())
 cartasAlice = list(map(int, input().split()))
 cartasBeatriz = list(map(int, input().split()))
@@ -10,7 +9,6 @@ cartasTrocadas = []
 trocas = 0
 
 # Processamento
-
 menorConjunto = min(A, B)
 
 for i in range(menorConjunto):
@@ -19,7 +17,5 @@ for i in range(menorConjunto):
         if cartasAlice[i] not in cartasTrocadas:
             cartasTrocadas.append(cartasAlice[i])
 
-
 #Saída
-
 print(trocas)
