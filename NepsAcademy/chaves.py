cab88cdec9492bb21cd46eca3b6eb81e85204658
MatheusNespolo <nespolo.m1@gmail.com>
2025-06-12 11:04:53 -0300

    ainda não foi
    Mesmo passando nos testes. Vou debuggar. Commit e era isso!

--- NepsAcademy/chaves.py
+++ NepsAcademy/chaves.py
@@ -3,29 +3,24 @@
 
 def confere_chaves():
     #Entrada
-    N = int(input())
-    codigo = []
-    linhas_validadas = 0
+    linhas = int(input())
+    codigo = str()
+    validacao = False
 
     #Processamento
-    for _ in range(N):
+    for _ in range(linhas):
         linha_do_codigo = str(input())
-        codigo.append(linha_do_codigo)
+        codigo += linha_do_codigo
 
-    for linha_do_codigo in codigo:
-        chaves_abertas = 0
-        chaves_fechadas = 0
-        if '{' not in linha_do_codigo and '}' not in linha_do_codigo:
-            linhas_validadas += 1
-        if linha_do_codigo.count('{') > 0:
-            chaves_abertas += linha_do_codigo.count('{')
-        if linha_do_codigo.count('}') > 0:
-            chaves_fechadas += linha_do_codigo.count('}')
-        else:
-            linhas_validadas = linhas_validadas
+    if '{' not in codigo and '}' not in codigo:
+        validacao = True
+    if codigo.count('{') == codigo.count('}'):
+        validacao = True
+    else:
+        validacao = False
         
     #Saída
-    if linhas_validadas == N:
+    if validacao:
         print('S')
     else:
         print('N')
