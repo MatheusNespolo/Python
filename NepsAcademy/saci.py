5cb22b47fd2091802b118af80446c6a8f1628251
MatheusNespolo <nespolo.m1@gmail.com>
2025-06-17 14:27:32 -0300

    Atualizei uma parte vo implementar melhor a lógica
    Preciso muito ir no banheiro. Commit e era isso!

--- NepsAcademy/saci.py
+++ NepsAcademy/saci.py
@@ -1,3 +1,34 @@
 #Toca do Saci
 #https://neps.academy/br/exercise/57
 
+#Entrada
+N, M = map(int, input().split())
+toca = []
+colunaEmilia = 0
+linhaEmilia = 0
+
+for i in range(N):
+    linha = list(map(int, input().split()))
+    if 2 in linha:
+        colunaEmilia = linha.index(2)
+        linhaEmilia = i
+    else:
+        colunaEmilia = colunaEmilia
+    toca.append(linha)
+
+#Processamento
+salasVisitadas = deque()
+distancia = 1
+salasVisitadas.append((linhaEmilia, colunaEmilia))
+while salasVisitadas:
+    linha, coluna = salasVisitadas.popleft()
+    if toca[linha][coluna] == 1: #Verifica se é um vizinho válido
+        salasVisitadas.append((linha, coluna))
+        distancia += 1
+        toca[linha][coluna] = 0
+    elif toca[linha][coluna] == 3:
+        distancia += 1
+        break
+
+#Saída
+print(distancia)
