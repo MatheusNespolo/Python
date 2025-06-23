325094d2302e83925c8ec9b736cb876c92b4c1ff
MatheusNespolo <nespolo.m1@gmail.com>
2025-06-23 13:28:31 -0300

    Implementa busca por vendedor de manga em grafo
    Adiciona um grafo de pessoas e utiliza busca em largura para encontrar um vendedor de manga, identificado por nomes terminados com 'm'. Inclui lógica de fila de pesquisa e função de verificação. Commit e era isso!

--- Algoritmos/Capítulo06/manga.py
+++ Algoritmos/Capítulo06/manga.py
@@ -1,1 +1,30 @@
 #Vendedor de manga
+
+#Importando módulos
+from collections import deque
+
+grafo = {}
+
+grafo['Matheus'] = ['Tom', 'Joao', 'Maria']
+grafo['Tom'] = ['Matheus', 'Joao', 'Maria']
+grafo['Joao'] = ['Matheus', 'Tom', 'Maria']
+grafo['Maria'] = ['Matheus', 'Tom', 'Joao']
+grafo['Antonio'] = []
+grafo['Roberto'] = []
+grafo['Alfredo'] = []
+
+fila_de_pesquisa = deque()
+fila_de_pesquisa += grafo['Matheus']
+
+def pessoa_e_vendedor(nome):
+    return nome[-1] == 'm'
+
+while fila_de_pesquisa:
+    pessoa = fila_de_pesquisa.popleft()
+    if pessoa_e_vendedor(pessoa):
+        print(pessoa, 'é um vendedor de manga.')
+        break
+    else:
+        fila_de_pesquisa += grafo[pessoa]
+        print('Não há um vendedor de manda')
+        break
