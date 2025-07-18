bb05fabae0fbc2c5e44b5f0126a6fcbc7fe1cdfd
MatheusNespolo <nespolo.m1@gmail.com>
2025-07-18 14:04:40 -0300

    Update aeroporto.py

--- NepsAcademy/aeroporto.py
+++ NepsAcademy/aeroporto.py
@@ -1,30 +1,33 @@
 #Aeroporto
 #https://neps.academy/br/exercise/290
 
-#Entrada
-A, V = map(int, input().split())
-contador = V
-# A = Quantidade de aeroportos
-# V = Quantidade de voos
-voosPorAeroporto = {}
 testes = 0
 aeroportosCongestionados = []
-for i in range(1, A + 1):
-    voosPorAeroporto[i] = 0
+voosPorAeroporto = {}
+def entrada():
+    A, V = map(int, input().split())
+    # A = Quantidade de aeroportos
+    # V = Quantidade de voos
+    for i in range(1, A + 1):
+        voosPorAeroporto[i] = 0
+    return A, V
+aeroportos, voos = entrada()
 
 #Processamento
-while A != 0 and V != 0:
+contador = voos
+while aeroportos != 0 and voos != 0:
     aeroporto1, aeroporto2 = map(int, input().split())
     voosPorAeroporto[aeroporto1] += 1
     voosPorAeroporto[aeroporto2] += 1
     contador -= 1
     if contador == 0:
         testes += 1
-        for j in voosPorAeroporto:
-            if voosPorAeroporto[j] == max(voosPorAeroporto.values()):
-                aeroportosCongestionados.append(j)
         break
-    
+
+for j in voosPorAeroporto:
+            if voosPorAeroporto[j] == max(voosPorAeroporto.values()):
+                aeroportosCongestionados.append(j)    
+ 
 #Saída
 for k in range(testes):
     print(f'Teste {k + 1}')
