d8ccaaf88f04236c7b96ddcdb2c3014e1e2eef2e
MatheusNespolo <nespolo.m1@gmail.com>
2025-07-18 09:14:55 -0300

    uma estrutura de código muito boa até agora
    Preciso armazenar qual aeroporto é o mais utilizado em cada teste a partir do dicionário. Vou verificar como destacar o maior valor a partir da estrutura que eu tenho e posteriormente mostrar em uma outra estrutura na saída. Commit e era isso!

--- NepsAcademy/aeroporto.py
+++ NepsAcademy/aeroporto.py
@@ -1,3 +1,27 @@
 #Aeroporto
 #https://neps.academy/br/exercise/290
 
+#Entrada
+A, V = map(int, input().split())
+contador = V
+# A = Quantidade de aeroportos
+# V = Quantidade de voos
+voosPorAeroporto = {}
+testes = 0
+for i in range(1, A):
+    voosPorAeroporto[i] = 0
+
+#Processamento
+while A != 0 and V != 0:
+    aeroporto1, aeroporto2 = map(int, input().split())
+    voosPorAeroporto[aeroporto1] += 1
+    voosPorAeroporto[aeroporto2] += 1
+    contador -= 1
+    if contador == 0:
+        testes += 1
+        break
+    
+#Saída
+for j in range(testes):
+    print(f'Teste {j+1}')
+print(voosPorAeroporto)
