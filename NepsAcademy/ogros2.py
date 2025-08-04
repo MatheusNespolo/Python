e41d7e21a6ddbafc2446e1c4b182cb24d49de90e
MatheusNespolo <nespolo.m1@gmail.com>
2025-08-04 13:02:11 -0300

    nada de nada
    vo pro SQL. Vai render mais talvez. Commit e era isso!

--- NepsAcademy/ogros2.py
+++ NepsAcademy/ogros2.py
@@ -2,10 +2,14 @@
 # https://neps.academy/br/exercise/261
 
 # Entrada
-
+faixas_de_premiacao, ogros = map(int, input().split())
+limites_de_forca = [(input().split())]
+premiacao_por_limite = [(input().split())]
+forcas = [(input().split())]
 
 # Processamento
+premiacao_por_forca = []
 
 
 # Saída
-
+print(premiacao_por_forca)
