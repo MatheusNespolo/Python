e3f7991712d38fc7d8de3b0890c8c3ff2b3f8050
MatheusNespolo <nespolo.m1@gmail.com>
2025-10-15 09:14:05 -0300

    nada d nada
    Commit e era isso!

--- NepsAcademy/tesouro.py
+++ NepsAcademy/tesouro.py
@@ -1,3 +1,12 @@
 # Divisão do Tesouro
 # https://neps.academy/br/exercise/976
 
+A = int(input())
+# Número de moedas na arca
+N = int(input())
+# Número de marinheiros (sem contar o capitão)
+CapitaoOlhoRoxo = 0
+premioMarinheiro = (A / N)
+while CapitaoOlhoRoxo < (2 * premioMarinheiro):
+    premioMarinheiro -= 1
+    CapitaoOlhoRoxo += 1
