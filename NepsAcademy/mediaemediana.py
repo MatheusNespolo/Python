44b1e7a9453aa392cb2b0038653170f920f24001
MatheusNespolo <nespolo.m1@gmail.com>
2026-02-18 17:42:14 -0300

    passou em dois casos teste
    Commit e era isso!

--- NepsAcademy/mediaemediana.py
+++ NepsAcademy/mediaemediana.py
@@ -2,28 +2,25 @@
 
 # https://neps.academy/br/exercise/1656
 
-# A média de três números inteiros A, B e C é (A + B + C)/3. A mediana de três npúmeros inteiros é o número que ficaria no meio se os três números fossem ordenados em ordem crescente.
+# A média de três números inteiros A, B e C é (A + B + C)/3. A mediana de três números inteiros é o número que ficaria no meio se os três números fossem ordenados em ordem crescente.
 
 # Sua tarefa é escrever um programa que, dados dois números inteiros distintos A e B, calcule o menor inteiro possível C tal que a média e a mediana de A, B e C sejam iguais. 
 
 # Entrada
 A, B = map(int, input().split())
-
-C = 0
+C = sorted([A,B])[1]
 
 # Processamento
 media = (A + B + C) / 3
-
-mediana = (sorted([A, B, C]))[1]
+mediana = sorted([A,B,C])[1]
 
 while media != mediana:
-    if mediana > media:
+    if mediana < media:
         C -= 1
-    else:
+    elif mediana > media:
         C += 1
-        media = (A + B + C) / 3
-        mediana = (sorted([A, B, C]))[1]
-
+    media = (A + B + C) / 3
+    mediana = sorted([A,B,C])[1]
 
 # Saída - deve produzir uma única linha, contendo um único número, o menor inteiro possível C tal que a média e a mediana de A, B e C sejam iguais.
 
